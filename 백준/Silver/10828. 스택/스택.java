7bba0ec9f82882e61e4d808996db5fc890c82690
cha-hyunwoo <2471005@hansung.ac.kr>
2025-12-19 15:55:47 +0900

    [Silver IV] Title: 스택, Time: 312 ms, Memory: 19988 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack=new Stack<>();
+
+
+        for(int i=0;i<n;i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            String command=st.nextToken(); // 명령어
+
+            switch(command){
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+
+                case "pop":
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                    }else{
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+
+                case "empty":
+                    System.out.println(stack.isEmpty()?1:0);
+                    break;
+                case "top":
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                    }else{
+                        System.out.println(stack.peek());
+                    }
+                    break;
+            }
+        }
+    }
+}
