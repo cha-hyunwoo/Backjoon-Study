f2b65226d85ebd8b0cd0457c585fa65b20361e8d
cha-hyunwoo <2471005@hansung.ac.kr>
2026-02-19 10:23:32 +0900

    [Silver III] Title: 주유소, Time: 292 ms, Memory: 30752 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+        int n=Integer.parseInt(br.readLine()); // 도시의 개수
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int []length=new int[n-1]; // 도로 사이의 길이
+        for(int i=0;i<n-1;i++){
+            length[i]=Integer.parseInt(st.nextToken());
+        }
+        st=new StringTokenizer(br.readLine());
+        int []cost=new int[n-1];
+        for(int i=0;i<n-1;i++){
+            cost[i]=Integer.parseInt(st.nextToken());
+        }
+        int min=cost[0]*length[0];
+        int lowCost=cost[0];
+        for(int i=1;i<n-1;i++){
+            if(lowCost>cost[i]){
+                lowCost=cost[i];
+            }
+            min+=lowCost*length[i];
+        }
+        System.out.print(min);
+    }
+}
