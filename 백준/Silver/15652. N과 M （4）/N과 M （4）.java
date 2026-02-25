c3e78a91a97f6dfbbb6ea45e64c708d086bd5849
cha-hyunwoo <2471005@hansung.ac.kr>
2026-02-25 13:14:33 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 17288 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int n,m;
+    public static int[]arr;
+    public static StringBuilder sb= new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+
+        arr=new int[m];
+        dfs(1,0);
+        System.out.print(sb);
+    }
+    public static void dfs(int at,int depth){
+        // 목표 개수만큼 다 뽑으면
+        if(depth==m) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=at;i<=n;i++){
+            arr[depth]=i; // 현재 깊이에 숫자 저장
+
+            dfs(i,depth+1);
+        }
+    }
+}
