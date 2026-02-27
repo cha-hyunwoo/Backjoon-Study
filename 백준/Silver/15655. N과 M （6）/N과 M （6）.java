6e1ec9ec9f6f08eb2e47da4eaf02e345319e5323
cha-hyunwoo <2471005@hansung.ac.kr>
2026-02-27 12:23:20 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int n,m;
+    public static int[]arr;
+    public static int[]nums; // 숫자 담을 곳
+    public static StringBuilder sb= new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+
+        nums=new int[n];
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            nums[i]=Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        arr=new int[m];
+        dfs(0,0);
+        System.out.print(sb);
+    }
+    public static void dfs( int at,int depth){
+        // 목표 개수만큼 다 뽑으면
+        if(depth==m) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=at;i<n;i++){
+                arr[depth]=nums[i];
+                dfs(i+1,depth+1);
+        }
+    }
+}
