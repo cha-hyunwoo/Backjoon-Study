b70e33700383733f7dda9cf9a978e893e0cf07e0
cha-hyunwoo <2471005@hansung.ac.kr>
2026-03-08 16:02:22 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 25524 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            int abs1 = Math.abs(o1);
+            int abs2 = Math.abs(o2);
+
+            if (abs1 == abs2) {
+                // 절댓값이 같으면 실제 숫자가 작은 놈이 우선
+                return o1 - o2;
+            }
+            // 절댓값이 다르면 절댓값 작은 놈이 우선
+            return abs1 - abs2;
+        });
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append('\n');
+                } else {
+                    sb.append(pq.poll()).append('\n');
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+        System.out.print(sb);
+    }
+}
