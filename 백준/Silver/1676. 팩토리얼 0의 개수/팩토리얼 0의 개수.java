dd73381f035d9dada547d3e5d84320a222c52c82
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-01 10:35:08 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n=Integer.parseInt(br.readLine());
+
+        int count=0;
+        while(n>=5){
+            count+=n/5;
+            n=n/5;
+        }
+        System.out.print(count);
+    }
+}
