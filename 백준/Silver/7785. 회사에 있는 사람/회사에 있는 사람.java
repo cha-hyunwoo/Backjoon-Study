2adcdd6c9ac6737729848ec4684228c9f8f88962
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-06 10:27:36 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 652 ms, Memory: 49244 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n=Integer.parseInt(br.readLine());
+
+        HashSet<String> set=new HashSet<>();
+        for(int i=0;i<n;i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            String name=st.nextToken();
+            String status=st.nextToken();
+
+            if(status.equals("enter")){
+                set.add(name);
+            }else{
+                set.remove(name);
+            }
+        }
+        List<String>list=new ArrayList<>(set);
+        Collections.sort(list,Collections.reverseOrder());
+
+        StringBuilder sb=new StringBuilder();
+        for(String name:list){
+            sb.append(name).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
